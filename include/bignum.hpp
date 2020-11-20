7902d1512667aa6bcf7e6bcac42b8bf020a711c8
Egor <30935034+eaniconer@users.noreply.github.com>
2020-11-21 02:28:16 +0300

    Support negative numbers.

--- include/bignum.hpp
+++ include/bignum.hpp
@@ -29,20 +29,35 @@ constexpr size_t strlen(const char* s) {
     return 1 + strlen(s + 1);
 }
 
-template <const char* s, class Base, class T = void>
+template <const char* s, size_t offset>
+struct StringView {
+    static constexpr const char* str = s + offset;
+};
+
+template <class StringView, class Base, Sign sign, class T = void>
 struct BigNumBuilder;
 
-template <const char* s, class Base, size_t... Is>
-struct BigNumBuilder<s, Base, std::integer_sequence<size_t, Is...>> {
-    using type = BigNum<Base, Sign::plus, (*(s + Is) - '0')...>;
+template <class StringView, class Base, Sign sign, size_t... Is>
+struct BigNumBuilder<StringView, Base, sign, std::integer_sequence<size_t, Is...>> {
+    using type = BigNum<
+        Base,
+        sign,
+        (*(StringView::str + Is) - '0')...>;
 };
 
 } // namespace utils
 
 template <const char* s, class Base = Base<10>>
 class BigNumBuilder {
+    static constexpr bool has_minus = (*s == '-');
+    static constexpr size_t offset = (has_minus ? 1 : 0);
+    using string_view = utils::StringView<s, offset>;
 public:
-    using type = typename utils::BigNumBuilder<s, Base, std::make_integer_sequence<size_t, utils::strlen(s)>>::type;
+    using type = typename utils::BigNumBuilder<
+        string_view,
+        Base,
+        has_minus ? Sign::minus : Sign::plus,
+        std::make_integer_sequence<size_t, utils::strlen(s + offset)>>::type;
 };
 
 } // namespace bignum
--- tests/test.cpp
+++ tests/test.cpp
@@ -10,29 +10,27 @@ using namespace bignum;
 
 namespace test1 {
 
-constexpr char rawNum1[] = "100";
-constexpr char rawNum2[] = "200";
-
-using Num1 = typename BigNumBuilder<rawNum1>::type;
-using Num2 = typename BigNumBuilder<rawNum2>::type;
-
-static_assert(std::is_same_v<Num1, BigNum<Base<10>, Sign::plus, 1, 0, 0>>);
-static_assert(std::is_same_v<Num2, BigNum<Base<10>, Sign::plus, 2, 0, 0>>);
+constexpr char rawNum[] = "100";
+using Num = typename BigNumBuilder<rawNum>::type;
+static_assert(std::is_same_v<Num, BigNum<Base<10>, Sign::plus, 1, 0, 0>>);
 
 } // namespace test1
 
 namespace test2 {
 
-constexpr char rawNum1[] = "100";
-constexpr char rawNum2[] = "200";
-
-using Num1 = typename BigNumBuilder<rawNum1, Base<10>>::type;
-using Num2 = typename BigNumBuilder<rawNum2, Base<10>>::type;
-
-static_assert(std::is_same_v<Num1, BigNum<Base<10>, Sign::plus, 1, 0, 0>>);
-static_assert(std::is_same_v<Num2, BigNum<Base<10>, Sign::plus, 2, 0, 0>>);
+constexpr char rawNum[] = "100";
+using Num = typename BigNumBuilder<rawNum, Base<10>>::type;
+static_assert(std::is_same_v<Num, BigNum<Base<10>, Sign::plus, 1, 0, 0>>);
 
 } // namespace test2
+
+namespace test3 {
+
+constexpr char rawNum[] = "-100";
+using Num = typename BigNumBuilder<rawNum, Base<10>>::type;
+static_assert(std::is_same_v<Num, BigNum<Base<10>, Sign::minus, 1, 0, 0>>);
+
+} // namespace test3
 
 int main() {
     return 0;
